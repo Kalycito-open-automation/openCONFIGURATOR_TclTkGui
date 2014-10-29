ad319648ecd70d32a0a53c3df929dea595e0d537
Ramakrishnan P <ramakrishanan_p@kalycito.com>
2014-10-29 13:15:30 +0530

    [FEATURE] Upgrade CMake packaging to generate platform dependent packages
     - Add support for x86/x64 txt2cdc under Linux
     - Add support for x86/x64 shared objects under Linux
     - Moved libs in platform specific folders
     - Adapted CMAKE to the new structure
     - Changed default Linux install to /opt/openCONFIGURATOR
     - Removed symlink in /usr/lib and added rpath to avoid linking problems
     - Changed symlink to openCONFIGURATOR executable to call a script to avoid that the resources are not found
     - Changed package name under Linux to openconfigurator
     - Adapted .desktop file accordingly and removed auto-generation of the file
     - Designed to create packages separately for each Ubuntu system
    
    Signed-off-by: Ramakrishnan P <ramakrishanan_p@kalycito.com>

--- linux/cmake/linux.cmake
+++ linux/cmake/linux.cmake
@@ -29,92 +29,38 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ################################################################################
 
+################################################################################
+# Set the package specific defaults
+################################################################################
 SET(DESTDIR "${CMAKE_BINARY_DIR}")
 SET(CPACK_SET_DESTDIR "ON")
 SET(CPACK_GENERATOR "DEB")
 SET(CPACK_SOURCE_GENERATOR "")
 
-IF(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "x86_64")
-    SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE amd64)
-    #SET(CPACK_RPM_PACKAGE_ARCHITECTURE "x86_64")
-ELSEIF(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "i686")
-    SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE i386)
-    #SET(CPACK_RPM_PACKAGE_ARCHITECTURE "i386")
-ENDIF(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "x86_64")
-
-SET(CPACK_PACKAGE_DEFAULT_LOCATION "/usr/share/${CPACK_PACKAGE_NAME}")
+SET(OPENCONF_LINUX_DIR "${OPENCONF_TCL_ROOT_DIR}/linux/")
+SET(CPACK_PACKAGE_DEFAULT_LOCATION "/opt/${CPACK_PACKAGE_NAME}")
 SET(CPACK_INSTALL_PREFIX "${CPACK_PACKAGE_DEFAULT_LOCATION}")
 
-SET(OPENCONF_LINUX_CONFIG_DIR "${OPENCONF_TCL_ROOT_DIR}/linux/configurations")
-SET(OPENCONF_LINUX_CMAKE_DIR "${OPENCONF_TCL_ROOT_DIR}/linux/cmake")
-
-
+SET(OPENCONF_LINUX_CONFIG_DIR "${OPENCONF_LINUX_DIR}/configurations")
+SET(OPENCONF_LINUX_CMAKE_DIR "${OPENCONF_LINUX_DIR}/cmake")
 SET(CPACK_DEBIAN_PACKAGE_HOMEPAGE "${PROJECT_HOMEPAGE}")
 SET(CPACK_DEBIAN_PACKAGE_MAINTAINER "${CPACK_PACKAGE_CONTACT}") #required
 SET(CPACK_DEBIAN_PACKAGE_DESCRIPTION "${CPACK_PACKAGE_DESCRIPTION_SUMMARY}")
-FILE(WRITE "${OPENCONF_LINUX_CONFIG_DIR}/${ORG_PROJECT_NAME}.desktop" "\n[Desktop Entry]\n"
-"Version=${CPACK_PACKAGE_VERSION}\n"
-"Encoding=UTF-8\n"
-"Name=openCONFIGURATOR\n"
-"Type=Application\n"
-"GenericName=openCONFIGURATOR\n"
-"Comment=Graphical User Interface for openCONFIGURATOR\n"
-"Exec=openCONFIGURATOR\n"
-"Icon=flair\n"
-"Categories=Development;\n"
-"GenericName[en_IN]=openCONFIGURATOR\n"
-"Name[en_IN]=openCONFIGURATOR")
 
-FILE (GLOB_RECURSE desktopfile "${OPENCONF_LINUX_CONFIG_DIR}/openCONFIGURATOR.desktop")
-INSTALL(FILES ${desktopfile} DESTINATION "/usr/share/applications")
+################################################################################
+# Find the system version, codename and id from /etc/lsb-release
+################################################################################
+EXECUTE_PROCESS(
+  COMMAND cat /etc/lsb-release
+  COMMAND grep DISTRIB_CODENAME
+  COMMAND awk -F= "{ print $2 }"
+  COMMAND tr "\n" " "
+  COMMAND sed "s/ //"
+  OUTPUT_VARIABLE LSB_CODE_NAME
+  RESULT_VARIABLE LSB_VER_RESULT
+)
+string(TOLOWER ${LSB_CODE_NAME} LSB_CODE_NAME)
 
-FILE (GLOB_RECURSE imgfile "${OPENCONF_IMAGES_DIR}/flair.svg")
-INSTALL(FILES ${imgfile} DESTINATION "/usr/share/pixmaps")
-
-#FIXME creating desktop-links but not removing..!!
-SET(CPACK_CREATE_DESKTOP_LINKS "/usr/share/applications/openCONFIGURATOR.desktop")
-
-#TODO update copyright files according to the format
-file(COPY ${OPENCONF_TCL_ROOT_DIR}/License.txt
-    DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
-FILE(RENAME "${CMAKE_CURRENT_BINARY_DIR}/License.txt" "${CMAKE_CURRENT_BINARY_DIR}/COPYRIGHT")
-INSTALL(FILES "${CMAKE_CURRENT_BINARY_DIR}/COPYRIGHT" DESTINATION "/usr/share/doc/${PROJECT_NAME}")
-
-file(COPY ${OPENCONF_LINUX_CMAKE_DIR}/postinst.in
-    DESTINATION ${CMAKE_CURRENT_BINARY_DIR}
-    FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
-    NO_SOURCE_PERMISSIONS)
-FILE(RENAME "${CMAKE_CURRENT_BINARY_DIR}/postinst.in" "${CMAKE_CURRENT_BINARY_DIR}/postinst")
-
-file(COPY ${OPENCONF_LINUX_CMAKE_DIR}/preinst.in
-    DESTINATION ${CMAKE_CURRENT_BINARY_DIR}
-    FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
-    NO_SOURCE_PERMISSIONS)
-FILE(RENAME "${CMAKE_CURRENT_BINARY_DIR}/preinst.in" "${CMAKE_CURRENT_BINARY_DIR}/preinst")
-
-file(COPY ${OPENCONF_LINUX_CMAKE_DIR}/postrm.in
-    DESTINATION ${CMAKE_CURRENT_BINARY_DIR}
-    FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
-    NO_SOURCE_PERMISSIONS)
-FILE(RENAME "${CMAKE_CURRENT_BINARY_DIR}/postrm.in" "${CMAKE_CURRENT_BINARY_DIR}/postrm")
-
-SET(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${CMAKE_CURRENT_BINARY_DIR}/postinst;${CMAKE_CURRENT_BINARY_DIR}/preinst;${CMAKE_CURRENT_BINARY_DIR}/postrm")
-
-SET(PACKAGE_EXCLUDE_PLATFORM ".dll|.bat|.exe|.desktop")
-
-SET(CPACK_STRIP_FILES "ON")
-
-#NEEDED               openCONFIGURATOR.so
-#NEEDED               libtcl8.5.so.0 / libtcl8.6.so.0
-#NEEDED               libstdc++.so.6
-#NEEDED               libc.so.6
-#NEEDED               libxml2.so.2
-#NEEDED               libgcc_s.so.1
-
-# objdump -x /usr/share/openconfigurator-tcltk/openCONFIGURATOR.so | grep NEEDED
-
-
-# Use the LSB stuff if possible
 EXECUTE_PROCESS(
   COMMAND cat /etc/lsb-release
   COMMAND grep DISTRIB_ID
@@ -124,6 +70,7 @@ EXECUTE_PROCESS(
   OUTPUT_VARIABLE LSB_ID
   RESULT_VARIABLE LSB_ID_RESULT
 )
+string(TOLOWER ${LSB_ID} LSB_ID)
 
 EXECUTE_PROCESS(
   COMMAND cat /etc/lsb-release
@@ -135,28 +82,105 @@ EXECUTE_PROCESS(
   RESULT_VARIABLE LSB_VER_RESULT
 )
 
-string(TOLOWER ${LSB_ID} LSB_ID)
-message("LSB output: ${LSB_ID_RESULT}:${LSB_ID} ${LSB_VER_RESULT}:${LSB_VER}")
-#if(NOT ${LSB_ID} STREQUAL "")
-#  # found some, use it :D
-#  set(INSTALLER_PLATFORM "${LSB_ID}-${LSB_VER}" CACHE PATH "Installer chosen platform")
-#else(NOT ${LSB_ID} STREQUAL "")
-#  set(INSTALLER_PLATFORM "linux-generic" CACHE PATH "Installer chosen platform")
-#endif(NOT ${LSB_ID} STREQUAL "")
-
-SET(CPACK_PACKAGE_FILE_NAME "${CMAKE_PROJECT_NAME}_${CPACK_PACKAGE_VERSION}-${LSB_ID}-${LSB_VER}-${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}")
-
-IF(${LSB_VER} STREQUAL "14.04")
-    SET(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6, libxss1, tcl, libtcl8.6 (>=8.6.0), tcl8.6 (>=8.6.0-2), tk8.6 (>= 8.6.0-2),  libtk8.6 (>= 8.6.0), libxml2 (>=2.7.8), tcl-thread, libstdc++6")
-ELSEIF(${LSB_VER} STREQUAL "12.04")
-    SET(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6, libxss1, tcl (>=8.5.0-2), tk (>=8.5.0-1), libxml2 (>=2.7.8), tclthread, libstdc++6")
+################################################################################
+# Determine the system architecture
+################################################################################
+IF(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "x86_64")
+    SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE amd64)
+ELSEIF(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "i386")
+    SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE i386)
+ELSEIF(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "i486")
+    SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE i386)
+ELSEIF(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "i586")
+    SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE i386)
+ELSEIF(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "i686")
+    SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE i386)
 ELSE()
- # TODO handle other versions of ubuntu
+    MESSAGE(FATAL_ERROR "Architecture ${CMAKE_SYSTEM_PROCESSOR} is not supported!")
+ENDIF(${CMAKE_SYSTEM_PROCESSOR} STREQUAL "x86_64")
+
+################################################################################
+# Copy the files required by openCONFIGURATOR library to resolve path.
+################################################################################
+FILE(COPY "${OPENCONF_LINUX_DIR}/tools/${LSB_CODE_NAME}/${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}/txt2cdc"
+    DESTINATION ${OPENCONF_TCL_ROOT_DIR}/resources
+    )
+FILE(COPY "${OPENCONF_LINUX_DIR}/libs/${LSB_CODE_NAME}/${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}/openCONFIGURATOR.so"
+    DESTINATION ${OPENCONF_TCL_ROOT_DIR}
+    )
+
+FILE(COPY "${OPENCONF_LINUX_DIR}/libs/${LSB_CODE_NAME}/${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}/openConfiguratorWrapper.so"
+    DESTINATION ${OPENCONF_LINUX_DIR}/libs/
+    )
+
+################################################################################
+# Excludes the files and folders from the repository to create the package
+################################################################################
+SET(PACKAGE_EXCLUDE_PLATFORM ".dll|.bat|.exe|.desktop|tools|configurations|precise|trusty|utopic")
+SET(CPACK_STRIP_FILES "ON")
+
+################################################################################
+# Set the debian output package file name
+################################################################################
+string(TOLOWER ${CMAKE_PROJECT_NAME} CMAKE_PROJECT_NAME_LOWER)
+SET(CPACK_PACKAGE_FILE_NAME "${CMAKE_PROJECT_NAME_LOWER}_${CPACK_PACKAGE_VERSION}-${LSB_ID}${LSB_VER}_${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}")
+
+
+################################################################################
+# Set all the dependencies for openCONFIGURATOR
+################################################################################
+IF((${LSB_VER} STREQUAL "14.04") OR (${LSB_VER} STREQUAL "14.10"))
+    SET(CPACK_DEBIAN_PACKAGE_DEPENDS "libboost-filesystem1.54.0, libboost-system1.54.0, tcl, libtcl8.6 (>=8.6.0), tcl8.6 (>=8.6.0-2), tk8.6 (>= 8.6.0-2), libtk8.6 (>= 8.6.0), tcllib, tklib, libxml2 (>=2.7.8), tcl-thread, libstdc++6, libc6, libxss1")
+ELSEIF((${LSB_VER} STREQUAL "12.04") OR (${LSB_VER} STREQUAL "10.04"))
+    SET(CPACK_DEBIAN_PACKAGE_DEPENDS "libc6, libxss1, tcl8.5 (>=8.5.0-2), tk8.5 (>=8.5.0-1), libxml2 (>=2.7.8), tclthread, libstdc++6, tcllib, tklib")
+ELSE()
+    MESSAGE(FATAL_ERROR "${LSB_ID} - ${LSB_CODE_NAME} - version ${LSB_VER} is not supported!")
 ENDIF()
-#SET(CPACK_DEBIAN_PACKAGE_RECOMMENDS "")
-#SET(CPACK_DEBIAN_PACKAGE_SUGGESTS "")
+
+################################################################################
+# Configure and install the configuration and license informations for the package
+################################################################################
+CONFIGURE_FILE(${OPENCONF_LINUX_CONFIG_DIR}/openCONFIGURATOR_unversioned.desktop
+            ${CMAKE_CURRENT_BINARY_DIR}/openCONFIGURATOR.desktop)
+INSTALL(FILES "${CMAKE_CURRENT_BINARY_DIR}/openCONFIGURATOR.desktop"
+    DESTINATION "/usr/share/applications")
+SET(CPACK_CREATE_DESKTOP_LINKS "/usr/share/applications/openCONFIGURATOR.desktop")
+
+CONFIGURE_FILE(${OPENCONF_LINUX_CONFIG_DIR}/package.info.conf ${OPENCONF_TCL_ROOT_DIR}/package.info)
+
+INSTALL(FILES "${OPENCONF_IMAGES_DIR}/flair.svg" DESTINATION "/usr/share/pixmaps")
+
+FILE(COPY ${OPENCONF_TCL_ROOT_DIR}/License.txt
+    DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
+FILE(RENAME "${CMAKE_CURRENT_BINARY_DIR}/License.txt" "${CMAKE_CURRENT_BINARY_DIR}/COPYRIGHT")
+
+INSTALL(FILES "${CMAKE_CURRENT_BINARY_DIR}/COPYRIGHT"
+    DESTINATION "/usr/share/doc/${PROJECT_NAME}")
 
 
-#CONFIGURE_FILE("${CMAKE_CURRENT_SOURCE_DIR}/cmake/postinst" "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}/scripts/postinst" @ONLY IMMEDIATE)
-#FILE(COPY ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}/scripts/postinst DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}
-#            FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+################################################################################
+# Copy and configure the control files for the debian package
+################################################################################
+FILE(COPY ${OPENCONF_LINUX_CMAKE_DIR}/postinst.in
+    DESTINATION ${CMAKE_CURRENT_BINARY_DIR}
+    FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
+    NO_SOURCE_PERMISSIONS)
+FILE(RENAME "${CMAKE_CURRENT_BINARY_DIR}/postinst.in" "${CMAKE_CURRENT_BINARY_DIR}/postinst")
+
+FILE(COPY ${OPENCONF_LINUX_CMAKE_DIR}/preinst.in
+    DESTINATION ${CMAKE_CURRENT_BINARY_DIR}
+    FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
+    NO_SOURCE_PERMISSIONS)
+FILE(RENAME "${CMAKE_CURRENT_BINARY_DIR}/preinst.in" "${CMAKE_CURRENT_BINARY_DIR}/preinst")
+
+FILE(COPY ${OPENCONF_LINUX_CMAKE_DIR}/postrm.in
+    DESTINATION ${CMAKE_CURRENT_BINARY_DIR}
+    FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
+    NO_SOURCE_PERMISSIONS)
+FILE(RENAME "${CMAKE_CURRENT_BINARY_DIR}/postrm.in" "${CMAKE_CURRENT_BINARY_DIR}/postrm")
+
+FILE(COPY "${OPENCONF_LINUX_DIR}/configurations/openCONFIGURATOR.sh"
+    DESTINATION ${OPENCONF_TCL_ROOT_DIR}
+    )
+
+SET(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${CMAKE_CURRENT_BINARY_DIR}/postinst;${CMAKE_CURRENT_BINARY_DIR}/preinst;${CMAKE_CURRENT_BINARY_DIR}/postrm")
